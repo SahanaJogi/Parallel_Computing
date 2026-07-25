ab7a914bca2107c38b13ef95a9df0a482bd219c9
SahanaJogi <jogisahana1782@gmail.com>
2026-07-25 13:49:43 +0530

    Create Test2.c

---
+++ Test2.c
@@ -0,0 +1,13 @@
+#include <omp.h>
+#include <stdio.h>
+int main()
+{
+    int n=12;
+    int data[8]={10,20,30,40,50,60,70,80};
+    #pragma omp parallel for
+    for(int i=0;i<n;i++)
+    {
+        int id=omp_get_thread_num();
+        printf("Thread %d processed index %d: value=%d\n",id,i,data[i]);
+    }
+}
